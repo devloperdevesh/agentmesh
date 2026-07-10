e96b85f27976f61a015ca08ad38bb146109e6c96
Devesh <214353321+devloperdevesh@users.noreply.github.com>
2026-07-10 14:29:57 +0530

    feat(control): implement workflow recovery

---
+++ internal/control/recovery.go
@@ -0,0 +1,16 @@
+package control
+
+
+func (c *Controller) Recover(id string) error{
+
+
+	cp,err:=c.RestoreCheckpoint(id)
+
+	if err!=nil{
+		return err
+	}
+
+
+	return c.Resume(cp.WorkflowID)
+
+}
